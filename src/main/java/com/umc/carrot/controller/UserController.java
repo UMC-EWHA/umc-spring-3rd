623e30b3d8f741c3f9a79ad5eecc8cb7b46022bd
Seoyoung <ryupark1006@gmail.com>
2022-11-06 14:22:36 +0900

    #1 Feat: Upload UserController

---
+++ src/main/java/com/umc/carrot/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.umc.carrot.controller;
+
+import com.umc.carrot.entity.User;
+import com.umc.carrot.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/user")
+    public Long create(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @GetMapping("/user/{id}")
+    public User read(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
+    @PostMapping("/user/{id}/update")
+    public Long update(@PathVariable Long id, @RequestBody User user) {
+        return userService.update(id, user);
+    }
+
+    @PostMapping("/user/{id}/delete")
+    public Long delete(@PathVariable Long id) {
+        userService.delete(id);
+        return id;
+    }
+}
