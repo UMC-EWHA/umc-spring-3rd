2d41bb1a6f3433535b04f266f9467f1d27caa94c
gkim <kimkm0419@naver.com>
2022-11-12 23:56:08 +0900

    #1 Refactor : Modify UserDao

--- src/main/java/com/umcewha/board/UserDao.java
+++ src/main/java/com/umcewha/board/UserDao.java
@@ -1,3 +1,5 @@
+//UserDao
+
 package com.umcewha.board;
 
 import com.umcewha.board.model.*;
@@ -6,35 +8,64 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class UserDao {
 
-    private JdbcTemplate jdbcTemplate;
+    public static List<User> users;
 
-    @Autowired
-    public void setDataSource(DataSource dataSource){
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    // jdbc 연결 실패해서 사용하는 임시 데이터
+    static {
+        users = new ArrayList<>();
+        users.add(new User(1,"testName1","1234"));
+        users.add(new User(2,"testName2","1234"));
+        users.add(new User(3,"testName3","1234"));
+        users.add(new User(4,"testName4","1234"));
+        users.add(new User(5,"testName5","1234"));
     }
 
-    public List<GetUserRes> userRes(){
-        return this.jdbcTemplate.query("Select * from User",
-                (rs, rowNum) -> new GetUserRes(
-                        rs.getInt("userIdx"),
-                        rs.getString("username"),
-                        rs.getString("password")
-                )
-        );
+    // JDBC
+//    private JdbcTemplate jdbcTemplate;
+//
+//    @Autowired
+//    public void setDataSource(DataSource dataSource) {
+//        this.jdbcTemplate = new JdbcTemplate(dataSource);
+//    }
+
+    // Select all user.
+    public List<User> getAllUsers() {
+        return users;
     }
 
-    public int addUser(PostUserReq postuserReq){
-        String createUserQuery = "insert into User (username, password) VALUES (?,?)";
-        Object[] createUserParams = new Object[]{
-                postuserReq.getUsername(), postuserReq.getPassword()
-        };
-        this.jdbcTemplate.update(createUserQuery, createUserParams);
+    // Select one user by userId
+    public User getUserByUsername(String username) {
+        return users
+                .stream()
+                .filter(user -> user.getUsername().equals(username))
+                .findAny()
+                .orElse(new User(-1, "", ""));
+    }
 
-        return this.jdbcTemplate.queryForObject("select last_insert_id()",int.class);
+    // Insert User
+    public User insertUser(User user) {
+        users.add(user);
+
+        return user;
+    }
+
+    // Modify User
+    public void updateUser(String username,User user) {
+        users.stream()
+                .filter(curUser -> curUser.getUsername().equals(username))
+                .findAny()
+                .orElse(new User(-1, "", ""))
+                .setPassword(user.getPassword());
+    }
+
+    // Delete User
+    public void deleteUser(String username) {
+        users.removeIf(user -> user.getUsername().equals(username));
     }
 }
