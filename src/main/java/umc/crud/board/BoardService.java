cb87771db7d38693ff19a1941e518afeda5f39c7
joonghyun <joongh@ewhain.net>
2022-11-12 17:35:27 +0900

    #16 Rename: Rename BoardService

--- src/main/java/umc/crud/board/BoardService.java
+++ src/main/java/umc/crud/board/BoardService.java
@@ -1,29 +1,29 @@
-package umc.crud.service;
+package umc.crud.board;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import umc.crud.dao.BoardDAO;
-import umc.crud.model.BoardDTO;
+import umc.crud.board.BoardDao;
+import umc.crud.board.model.BoardDto;
 import java.util.List;
 
 @Service
 public class BoardService {
     @Autowired
-    BoardDAO boardDao;
+    BoardDao boardDao;
 
-    public List<BoardDTO> getAllBoards() {
+    public List<BoardDto> getAllBoards() {
         return boardDao.getAllBoards();
     }
 
-    public BoardDTO getBoardByWriter(String writer) {
+    public BoardDto getBoardByWriter(String writer) {
         return boardDao.getBoardByWriter(writer);
     }
 
-    public BoardDTO writeBoard(BoardDTO board) {
+    public BoardDto writeBoard(BoardDto board) {
         return boardDao.insertBoard(board);
     }
 
-    public void modifyBoard(String writer, BoardDTO board) {
+    public void modifyBoard(String writer, BoardDto board) {
         boardDao.updateBoard(writer, board);
     }
 
