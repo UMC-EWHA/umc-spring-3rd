2a873dbcda8b5a74aef419ea8cd222a73229ea8d
Seoyoung <ryupark1006@gmail.com>
2022-11-14 19:12:29 +0900

    #16 Feat: Upload GetUserRes

---
+++ src/main/java/com/umc/board/model/GetUserRes.java
@@ -0,0 +1,16 @@
+package com.umc.board.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+// 회원 정보 조회 요청의 결과
+public class GetUserRes {
+    private int userIdx;
+    private String name;
+    private String email;
+    private String password;
+}
