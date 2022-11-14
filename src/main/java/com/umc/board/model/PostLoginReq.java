e4de2c0d0f697c7015ed15a5a4d4b82441b421b7
Seoyoung <ryupark1006@gmail.com>
2022-11-14 19:14:58 +0900

    #16 Feat: Upload PostLoginReq

---
+++ src/main/java/com/umc/board/model/PostLoginReq.java
@@ -0,0 +1,13 @@
+package com.umc.board.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+// 로그인을 위해 전달할 데이터 형태
+public class PostLoginReq {
+    private String email;
+    private String password;
+}
