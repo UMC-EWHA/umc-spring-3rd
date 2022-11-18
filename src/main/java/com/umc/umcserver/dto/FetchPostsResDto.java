1c191eb0939ce85d7f1555583868a493662b3771
hyeonjeong Park <py0429@ewhain.net>
2022-11-19 02:25:42 +0900

    [Feat] week 8 pagination api

--- src/main/java/com/umc/umcserver/dto/FetchPostsResDto.java
+++ src/main/java/com/umc/umcserver/dto/FetchPostsResDto.java
@@ -3,6 +3,9 @@ package com.umc.umcserver.dto;
 import com.umc.umcserver.repository.Board;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
@@ -11,6 +14,7 @@ public class FetchPostsResDto {
     private String title;
     private String content;
     private String writer;
+    private Pageable pageable;
 
 
     public FetchPostsResDto(Board entity) {
