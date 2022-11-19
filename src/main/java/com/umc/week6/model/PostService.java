ee8855a8d48c28cc24ca00495b6bcd061897b4fa
choiyounji <yunji71@naver.com>
2022-11-19 20:12:44 +0900

    #31 Feat : Add Transactional

--- src/main/java/com/umc/week6/model/PostService.java
+++ src/main/java/com/umc/week6/model/PostService.java
@@ -2,9 +2,11 @@ package com.umc.week6.model;
 import com.umc.week6.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
- 
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
- 
+
+@Transactional
 @Service
 public class PostService {
     @Autowired
