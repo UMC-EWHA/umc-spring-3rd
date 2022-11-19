6f58ba12ca7506f2eeb3f118e7e834ad9e026d9e
Seoyoung <ryupark1006@gmail.com>
2022-11-19 22:03:06 +0900

    #16 Style: Fix typo in PatchUserReq

--- src/main/java/com/umc/board/model/PatchUserReq.java
+++ src/main/java/com/umc/board/model/PatchUserReq.java
@@ -8,6 +8,6 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 // 회원 정보 수정을 위해 전달할 데이터
 public class PatchUserReq {
-    private int userId;
+    private int userIdx;
     private String name;
 }
