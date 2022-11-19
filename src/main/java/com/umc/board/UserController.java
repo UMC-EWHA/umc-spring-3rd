803399370a1b510d59eae8d3f6d548ec66d26ed3
Seoyoung <ryupark1006@gmail.com>
2022-11-19 23:47:57 +0900

    #16 Refactor: Update UserController

--- src/main/java/com/umc/board/UserController.java
+++ src/main/java/com/umc/board/UserController.java
@@ -1,32 +1,75 @@
 package com.umc.board;
 
-import lombok.RequiredArgsConstructor;
+import com.umc.board.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RequiredArgsConstructor
+import java.util.List;
+
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
+    @Autowired
     private final UserService userService;
 
-    @PostMapping("/user")
-    public Long create(@RequestBody User user) {
-        return userService.save(user);
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
-    @GetMapping("/user/{id}")
-    public User read(@PathVariable Long id) {
-        return userService.findById(id);
+    // 회원 가입 API
+    @ResponseBody
+    @PostMapping("/sign-up")
+    public PostUserRes createUser(@RequestBody PostUserReq postUserReq) {
+        try {
+            return userService.createUser(postUserReq);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @PostMapping("/user/{id}/update")
-    public Long update(@PathVariable Long id, @RequestBody User user) {
-        return userService.update(id, user);
+    // 로그인 API
+    @ResponseBody
+    @PostMapping("/log-in")
+    public PostLoginRes logIn(@RequestBody PostLoginReq postLoginReq) {
+        try {
+            return userService.logIn(postLoginReq);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @PostMapping("/user/{id}/delete")
-    public Long delete(@PathVariable Long id) {
-        userService.delete(id);
-        return id;
+    // 전체 유저 정보 조회 API
+    @ResponseBody
+    @GetMapping("")
+    public List<GetUserRes> getUsers() {
+        try {
+            return userService.getUsers();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // 유저 아이디로 정보 조회 API
+    @ResponseBody
+    @GetMapping("/{userIdx}")
+    public GetUserRes getUser(@PathVariable("userIdx") int userIdx) {
+        try {
+            return userService.getUser(userIdx);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // 회원 정보 수정 API
+    @ResponseBody
+    @PatchMapping("/{userIdx}")
+    public String modifyUser(@PathVariable("userIdx") int userIdx, @RequestBody User user) {
+        try {
+            userService.modifyUser(new PatchUserReq(userIdx, user.getName()));
+            return "회원 정보가 수정되었습니다.";
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
