173eaaf9bca0a1f245bb2a21430a60472c0e85a7
joonghyun <joongh@ewhain.net>
2022-11-24 19:40:08 +0900

    #31 Feat: Upload models

---
+++ src/main/java/umc/crud/src/user/model/PostLoginRes.java
@@ -0,0 +1,12 @@
+package umc.crud.src.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PostLoginRes {
+    private int userIdx;
+}
